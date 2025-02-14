f677d4c63f5900d919966a5240feb88ca949db04
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-02-14 18:14:17 -0300

    feat: update requestData function to return a list of dynamic data

--- assincronismo/bin/main.dart
+++ assincronismo/bin/main.dart
@@ -7,7 +7,7 @@ void main() async {
   // requestReceitas();
 }
 
-Future<void> requestData() async {
+Future<List<dynamic>> requestData() async {
   String url =
       "https://gist.githubusercontent.com/DanielBrown1998/2e08120282bc3f5c4160b6ba56b2cef1/raw/528d5e9dcafd230b537d7ef48dd1f35da638aa5a/accounts.json";
   http.Response response = await http.get(Uri.parse(url));
@@ -17,6 +17,7 @@ Future<void> requestData() async {
   for (Map<String, dynamic> item in file) {
     print(item);
   }
+  return file;
 }
 
 Future<void> requestReceitas() async {
