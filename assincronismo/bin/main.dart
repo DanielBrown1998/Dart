52ef8f40d7bdcd9a4bee0eee441872cbd20c35bc
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-02-19 13:10:26 -0300

    feat: implement sendData function to post account data and update requestData

--- assincronismo/bin/main.dart
+++ assincronismo/bin/main.dart
@@ -1,23 +1,57 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:assincronismo/api_key.dart' as key;
 
 void main() async {
   requestData();
-  // requestReceitas();
+  sendData();
 }
 
-Future<List<dynamic>> requestData() async {
+requestData() async {
   String url =
       "https://gist.githubusercontent.com/DanielBrown1998/2e08120282bc3f5c4160b6ba56b2cef1/raw/528d5e9dcafd230b537d7ef48dd1f35da638aa5a/accounts.json";
   http.Response response = await http.get(Uri.parse(url));
-  print(response.request?.method);
-  print(response.statusCode);
   List<dynamic> file = json.decode(response.body);
   for (Map<String, dynamic> item in file) {
     print(item);
   }
-  return file;
+}
+
+sendData({Map<String, dynamic>? mapAccount}) async {
+  String url = 'https://api.github.com/gists/2e08120282bc3f5c4160b6ba56b2cef1';
+
+  mapAccount ??= {
+      "id": "NewID001",
+      "name": "NewName",
+      "lastName": "NewLastName",
+      "balance": 0.0,
+    };
+
+  String gist =
+      "https://gist.githubusercontent.com/DanielBrown1998/2e08120282bc3f5c4160b6ba56b2cef1/raw/528d5e9dcafd230b537d7ef48dd1f35da638aa5a/accounts.json";
+  http.Response responseGet = await http.get(Uri.parse(gist));
+  print("GET: ${responseGet.request?.method}");
+  print("GIST STATUS: ${responseGet.statusCode}");
+  List<dynamic> file = json.decode(responseGet.body);
+
+  file.add(mapAccount);
+  http.Response response = await http.post(Uri.parse(url),
+      body: json.encode({
+        'description': 'Accounts.json',
+        'public': true,
+        'files': {
+          'accounts.json': {
+            'content': json.encode(file),
+          }
+        }
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${key.githubApiKey}',
+      });
+      print("POST: ${response.request?.method}");
+  print("SAVE STATUS ${response.statusCode}");
 }
 
 Future<void> requestReceitas() async {
