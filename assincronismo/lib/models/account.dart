c22eb0dffa9a343e900329015b9120963867cce5
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-02-21 15:24:02 -0300

    feat: add Account model with serialization and deserialization methods

---
+++ assincronismo/lib/models/account.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+class Account {
+  String id;
+  String name;
+  String lastname;
+  String balance;
+
+  Account({
+    required this.id,
+    required this.name,
+    required this.lastname,
+    required this.balance,
+  });
+
+  factory Account.fromMap(Map<String, dynamic> map) {
+    return Account(
+      id: map['id'],
+      name: map['name'],
+      lastname: map['lastname'],
+      balance: map['balance'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'lastname': lastname,
+      'balance': balance,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Account{id: $id, name: $name, lastname: $lastname, balance: $balance}';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Account &&
+        other.id == id &&
+        other.name == name &&
+        other.lastname == lastname &&
+        other.balance == balance;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ lastname.hashCode ^ balance.hashCode;
+  }
+
+  Account copyWith({
+    String? id,
+    String? name,
+    String? lastname,
+    String? balance,
+  }) {
+    return Account(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      lastname: lastname ?? this.lastname,
+      balance: balance ?? this.balance,
+    );
+  }
+
+  toJson() {
+    return json.encode({
+      'id': id,
+      'name': name,
+      'lastname': lastname,
+      'balance': balance,
+    });
+  }
+
+  fromJson(String jsonFile) {
+    var map = json.decode(jsonFile);
+    id = map['id'];
+    name = map['name'];
+    lastname = map['lastname'];
+    balance = map['balance'];
+  }
+}
