4cbcf43564cce5b73a76a4f791376fac9d8ec660
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-04-25 16:12:32 -0300

    update code for codewars kata

--- collections/bin/collections.dart
+++ collections/bin/collections.dart
@@ -1,12 +1,48 @@
-import 'package:collections/exceptions/exceptions.dart';
+int sumOfIntervals(List<List<int>> intervals) {
+  int value = 0;
 
-void main() {
-  try {
-    Delivery.checkDelivery(false);
-  } on DeliveryFailedException catch (erro) {
-    print(erro.msg);
-    print(erro.deliveryId);
-    print(erro.driverName);
+  //ordenando os intervalos
+  intervals.sort(
+    (a, b) => a[0] != b[0] ? a[0].compareTo(b[0]) : a[1].compareTo(b[1]),
+  );
+  //inicializando o intervalo
+  int min_value = intervals[0][0];
+  int max_value = intervals[0][1];
+  value = max_value - min_value;
+
+  for (int i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] >= max_value) {
+      value += intervals[i][1] - intervals[i][0];
+      max_value = intervals[i][1];
+    } else if (intervals[i][0] <= max_value) {
+      if (intervals[i][1] < max_value) {
+        continue;
+      } else {
+        value += intervals[i][1] - max_value;
+        max_value = intervals[i][1];
+      }
+    }
   }
 
+  return value;
+}
+
+void main() {
+  List<List<int>> intervals = [
+    [1, 5],
+    [1, 3],
+    [10, 20],
+    [1, 6],
+    [2, 3],
+    [4, 5],
+    [6, 7],
+    [8, 9],
+    [10, 11],
+    [12, 13],
+    [14, 15],
+    [16, 17],
+    [18, 19],
+  ];
+
+  print(sumOfIntervals(intervals)); // Output: 17;
 }
