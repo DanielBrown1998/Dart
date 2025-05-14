7f220ef0d1dd47bd625fdb94f6ff91a7dca24f59
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-05-14 18:10:45 -0300

    praticando dart

--- notas/bin/main.dart
+++ notas/bin/main.dart
@@ -62,6 +62,50 @@ String formatDuration(int totalSeconds) {
   return result;
 }
 
+bool isPrime(int number) {
+  if (number <= 1) {
+    return false; // 1 e números negativos não são primos
+  }
+  if (number == 2) return true;
+  if (number % 2 == 0) return false;
+  for (int i = 3; i * i <= number; i += 2) {
+    if (number % i == 0) {
+      return false; // Se for divisível por algum número, não é primo
+    }
+  }
+  return true; // Se não for divisível por nenhum, é primo
+}
+
+String primeFactors(int n) {
+  String num = "";
+  int cont = 0;
+  String part = "";
+  if (n % 2 == 0) {
+    while (n % 2 == 0) {
+      n = n ~/ 2;
+      cont++;
+    }
+    part += cont != 1 ? "(2**$cont)" : "(2)";
+  }
+  num += part;
+  part = "";
+  cont = 0;
+  for (int i = 3; i <= n; i += 2) {
+    if (isPrime(i) && n % i == 0) {
+      while (n % i == 0) {
+        n = n ~/ i;
+        cont++;
+      }
+      part = cont != 1 ? "($i**$cont)" : "($i)";
+      cont = 0;
+    }
+    num += part;
+    part = "";
+  }
+  return num;
+}
+
 void main() {
-  print(formatDuration(132030240));
+  print(primeFactors(2 * 2 * 2 * 2 * 2 * 2 * 3 * 3 * 3 * 5 * 7 * 11 * 19));
+  // print(formatDuration(132030240));
 }
